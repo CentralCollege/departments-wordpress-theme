246e9b5080d42918eda9fb0dce1b958fae64d084
GCashMoney <macdonaldg1@central.edu>
2014-03-27 12:00:33 -0500

    Fixed sprite and added permalink to title.

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
 	//Loop starts here
 	if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <div class="post-entry">
-    <?php the_title( '<h2>', '</h2>' ); ?>
+    <h2><a href="<?php the_permalink() ?>" ><?php the_title(); ?></a></h2>
     
     <?php the_excerpt(); ?>
     
-    <a href="<?php the_permalink() ?>" >Read more</a>
+    
+    <a href="<?php the_permalink() ?>" class="read-more">Read more</a>
     <div class="clearboth"></div>
     </div>
 
