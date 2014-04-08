94d3bf0205c3fe727be44614b4d99b2845317714
GCashMoney <macdonaldg1@central.edu>
2014-04-08 15:00:56 -0500

    Copied code from single.php into page.php. Also added a few styles for page and single, still need to mess with the images in the posts.

---
+++ page.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+
+	<div class="posts">
+	<?php
+	//Loop starts here
+	if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div class="post-content">
+    <h2><a href="<?php the_permalink() ?>" ><?php the_title(); ?></a></h2>
+    
+    <?php the_time( get_option( 'date_format' ) ) ?>
+    
+    <?php the_author_posts_link() ?> 
+    
+    <?php the_content(); ?>
+    
+    <div class="clearboth"></div>
+    </div>
+
+	<?php 
+    //Loop ends here
+    endwhile; else: ?>
+    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+    <?php endif; ?>
+    </div>
+	
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -5,7 +5,7 @@
 	<?php
 	//Loop starts here
 	if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <div class="post-entry">
+    <div class="post-content">
     <h2><a href="<?php the_permalink() ?>" ><?php the_title(); ?></a></h2>
     
     <?php the_time( get_option( 'date_format' ) ) ?>
