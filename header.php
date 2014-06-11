71cc9263d9f14c839be86660849f2a5bf8c963e5
jacoboyen <oyenj@central.edu>
2014-06-11 09:26:31 -0500

    Added option for no header image

--- header.php
+++ header.php
@@ -6,16 +6,23 @@
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
     	<?php wp_head(); ?>
     </head>
-    <body> 
-    	<a href="<?php echo home_url(); ?>">
-        	<div class="logoGroup">	
+    <body>
+    	<?php if (get_header_image() != ''){ ?> 
+            <a href="<?php echo home_url(); ?>">
+                <div class="logoGroup">	
+                    <div class="logo"><img alt="Central College Logo" src="//d1lqhpmxg10s5j.cloudfront.net/images/main/centralCollegeLogo.png"></div>
+                    <h1><?php bloginfo('name'); ?></h1>   
+                </div>
+            </a> 
+            <div class="coverImage">
+                <img src="<?php header_image(); ?>" />
+            </div>
+        <?php }else{?>
+			<div class="logoGroupNoImage">	
                 <div class="logo"><img alt="Central College Logo" src="//d1lqhpmxg10s5j.cloudfront.net/images/main/centralCollegeLogo.png"></div>
                 <h1><?php bloginfo('name'); ?></h1>   
-        	</div>
-        </a> 
-        <div class="coverImage">
-            <img src="<?php header_image(); ?>" />
-        </div>
+            </div>			
+		<?php }?>
       
         <div id="wrapper">
             <div id="header">
