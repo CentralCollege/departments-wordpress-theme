1d24720ef04d8a45720cf28c94a963127720feb6
GCashMoney <macdonaldg1@central.edu>
2014-06-12 16:00:33 -0500

    Fixed anchor tags in post-content so that they are underlined, but the underline vanishes when hovered over.

--- header.php
+++ header.php
@@ -10,19 +10,23 @@
 		<?php if (!is_front_page() && get_option('active_banner_photo') == 'no') { 
 			
 		?> 
+        	<div class="logoLink">
              <a href="<?php echo home_url(); ?>">
                 <div class="logoGroupNoImage">	
                     <div class="logo"><img alt="Central College Logo" src="//d1lqhpmxg10s5j.cloudfront.net/images/main/centralCollegeLogo.png"></div>
                     <h1><?php bloginfo('name'); ?></h1>   
                 </div>
             </a>
+            </div>
         <?php }else{?>
+        	<div class="logoLink">
         	<a href="<?php echo home_url(); ?>">
                 <div class="logoGroup">	
                     <div class="logo"><img alt="Central College Logo" src="//d1lqhpmxg10s5j.cloudfront.net/images/main/centralCollegeLogo.png"></div>
                     <h1><?php bloginfo('name'); ?></h1>   
                 </div>
             </a> 
+            </div>
             <div class="coverImage">
                 <img src="<?php header_image(); ?>" />
             </div>
