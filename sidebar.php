5746f15c109e90c08b17342d34819e6c91cb101e
jacoboyen <oyenj@central.edu>
2014-06-23 16:05:54 -0500

    Changed sidebar so that it was more clear what was happening

--- sidebar.php
+++ sidebar.php
@@ -1,20 +1,14 @@
-<?php if (get_option('active_side_widget') == 'no') {	
-?>
-	<div class="Rightsidebar">
+<div class="Rightsidebar">
     	<?php get_search_form(); ?>
-        <?php dynamic_sidebar('central_sidebar_widgets') ?>
+        <?php if (get_option('active_side_nav') == 'no') {	
+	        dynamic_sidebar('central_sidebar_widgets');
+		}else{?> 
+        <ul class="pagenav">
+    	<?php 
+	        $args = array('title_li'     => __(''));
+            wp_list_pages($args); 
+		?>
+		</ul> 	           
+		<?php dynamic_sidebar('central_sidebar_widgets');?>
     </div>
-<?php }else{?>
-	<div class="Rightsidebar">    
-    <ul class="pagenav">
-    <?php get_search_form(); ?> 
-    <?php 
-        $args = array(
-            'title_li'     => __('')
-        );
-            wp_list_pages($args); ?></ul> 	
-            
-    
-    <?php dynamic_sidebar('central_sidebar_widgets') ?>
-	</div>
  <?php }?>
