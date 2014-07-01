267d8d96e8b8f392625c6ceafacd54087c774682
jacoboyen <oyenj@central.edu>
2014-07-01 08:19:07 -0500

    Updated dropdown navigation for G$ to work with

--- header.php
+++ header.php
@@ -6,15 +6,74 @@
     	<title><?php wp_title( '-', true, 'right' ); ?> <?php bloginfo('name'); ?></title>
         <link href='http://fonts.googleapis.com/css?family=Cagliostro' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+        <style type="text/css">
+		#cui_menu ul {
+			margin: 0;
+			padding: 0;
+			list-style: none;
+			font-size: 1.1em
+		}
+		#cui_menu ul li {
+			display: block;
+			position: relative;
+			float: left;
+			border-right: 1px solid #cb2026;
+		}
+		#cui_menu ul li:last-child {
+			border-right: none;
+		}
+		#cui_menu li ul {
+			display: none;
+		}
+		#cui_menu ul li a {
+			display: block;
+			text-decoration: none;
+			color: #ffffff;
+			padding: 5px 15px 5px 15px;
+			background: #990000;
+			white-space: nowrap;
+			font-size: 1.4em;
+		}
+		#cui_menu ul li a:hover {
+			background: #cb2026;
+		}
+		#cui_menu li:hover ul {
+			display: block;
+			position: absolute;
+		}
+		#cui_menu li:hover li {
+			float: none;
+		}
+		#cui_menu li:hover a {
+			background: #ccc;
+		}
+		#cui_menu li:hover li a:hover {
+			background: #333;
+		}
+		#cui_menu ul li ul li{
+			font-size: .5em;
+		}
+		@media only screen and (max-width: 480px) {
+			#cui_menu ul li {
+				float: none;
+				display: block;	
+				color: #fff;
+				border-bottom: 1px solid #fff;
+			}
+			#cui_menu ul li a {
+				background: none;
+				color: #fff;
+				padding: none;
+				white-space:normal;
+			}
+			#cui_menu ul li ul li{
+				display: none;
+			}	
+		}
+		</style>
     	<?php wp_head(); ?>
     </head>
-    <body>
-    	<div id="hamburger">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div> 
-        <div id="contentLayer"></div>
+    <body> 
 		<?php if (!is_front_page() && get_option('active_banner_photo') == 'no') { 
 			
 		?> 
@@ -42,9 +101,28 @@
       
         <div id="wrapper">
             <div id="header">
-                	 <?php
-						if ( has_nav_menu( 'header-menu' ) ) {
-                    		wp_nav_menu( array( 'menu' => 'header-menu', 'menu_class' => 'top-menu',) ); 
-						} ?>
+				<div id="cui_menu">
+                    <ul id="menu">
+                    <li><a href="#">Home</a></li>
+                    <li>
+                        <a href="#">About</a>
+                        <ul class="hidden">
+                            <li><a href="#">Who We Are</a></li>
+                            <li><a href="#">What We Do</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Portfolio</a>
+                        <ul class="hidden">
+                            <li><a href="#">Photography</a></li>
+                            <li><a href="#">Web & User Interface Design</a></li>
+                            <li><a href="#">Illustration</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                </div>
+                <div class="clearboth"></div>
             </div>
-            
+            <div id="content"></div>
