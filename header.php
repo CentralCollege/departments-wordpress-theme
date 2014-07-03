f997db28ae671277977dd086c4ca2b1bd8b9358f
GCashMoney <macdonaldg1@central.edu>
2014-07-03 10:34:51 -0500

    Added menu navigation.

--- header.php
+++ header.php
@@ -37,24 +37,12 @@
             <div id="header">
 				<div id="cui_menu">
                     <ul id="menu">
-                    <li><a href="#">Home</a></li>
-                    <li>
-                        <a href="#">About</a>
-                        <ul class="hidden">
-                            <li><a href="#">Who We Are</a></li>
-                            <li><a href="#">What We Do</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">Portfolio</a>
-                        <ul class="hidden">
-                            <li><a href="#">Photography</a></li>
-                            <li><a href="#">Web & User Interface Design</a></li>
-                            <li><a href="#">Illustration</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">Contact</a></li>
+                    	<?php
+						if ( has_nav_menu( 'header-menu' ) ) {
+
+                    		wp_nav_menu( array( 'menu' => 'header-menu', 'menu_class' => 'menu', ) ); 
+
+						} ?>
                 </ul>
                 </div>
                 <div class="clearboth"></div>
