b3fde67a0279d26a4ccce72d8af6c26056a1a6d8
Jacob Oyen <oyenj@central.edu>
2015-02-12 15:24:45 -0600

    Re-enabled widgets that were previosuly disabled. Solves #45.

--- functions.php
+++ functions.php
@@ -29,12 +29,8 @@
 	// Unregister default widgets
 	// ----------------------------------------------------------------
 	function unregister_default_wp_widgets(){
-		unregister_widget('WP_Widget_Pages');
 		unregister_widget('WP_Widget_Calendar');
-		unregister_widget('WP_Widget_Archives');
 		unregister_widget('WP_Widget_Meta');
-		unregister_widget('WP_Widget_Search');
-		unregister_widget('WP_Widget_Categories');
 		unregister_widget('WP_Widget_Recent_Posts');
 		unregister_widget('WP_Widget_Recent_Comments');
 		unregister_widget('WP_Widget_Tag_Cloud');
