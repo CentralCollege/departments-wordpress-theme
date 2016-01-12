ddefaea4f07388b37ae15730551593da16c1af3c
Jordan Bohr <bohrj1@central.edu>
2016-01-12 16:21:36 -0600

    Awaiting provision fix.

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+	<div class="posts">
+        <h1>Error 404</h1>
+        <h2>Page not found</h2>
+        <div id="breadcrumb"><a href="/">home</a> &frasl; error 404 - page not found</div>
+        <h4>Error 404 - The requested resource was not found</h4>
+<!--        <p>We're sorry, that page can't be found. We may have moved that page to a new location or removed it from our site.</p>
+        <h3>Perhaps you'd like to try the following:</h3>
+        <p>
+            <ul>
+                <li><a href="/human-resources/directory/">Find someone in the directory to contact</a></li>
+                <li><a href="http://search.central.edu">Search our site</a></li>
+                <li>Start over at the <a href="/">Central homepage</a></li>
+            </ul>
+        </p>
+        <p>&nbsp;</p>-->
+    </div>
+    
+	<?php get_sidebar(); ?>
+    
+<?php get_footer(); ?>
