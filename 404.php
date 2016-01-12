dfe8fcd1d2972397a36fdd0eed7f3ec9c40df401
Jordan Bohr <bohrj1@central.edu>
2016-01-12 17:02:27 -0600

    Basic 404 page for #57.

--- 404.php
+++ 404.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
-	<div class="posts">
-        <h1>Error 404</h1>
-        <h2>Page not found</h2>
-        <div id="breadcrumb"><a href="/">home</a> &frasl; error 404 - page not found</div>
-        <h4>Error 404 - The requested resource was not found</h4>
-<!--        <p>We're sorry, that page can't be found. We may have moved that page to a new location or removed it from our site.</p>
-        <h3>Perhaps you'd like to try the following:</h3>
+<div class="posts">
+    <div class="post-content">
+        <h2>Error 404</h2>
+        <h4>The requested page was not found</h4>
+        <hr />
+        <p>We're sorry, that page can't be found. We may have moved that page to a new location or removed it from our site.</p>
+		<strong>Perhaps you'd like to try the following:</strong>
         <p>
             <ul>
                 <li><a href="/human-resources/directory/">Find someone in the directory to contact</a></li>
@@ -14,9 +14,10 @@
                 <li>Start over at the <a href="/">Central homepage</a></li>
             </ul>
         </p>
-        <p>&nbsp;</p>-->
+        <p>&nbsp;</p>
+    	<div class="clearboth"></div>
     </div>
+</div>
     
 	<?php get_sidebar(); ?>
-    
 <?php get_footer(); ?>
