201d65d9827d925261ef448e0aaca84cb0670b9a
Jacob Oyen <oyenj@central.edu>
2016-03-11 09:40:10 -0600

    Latest template updates module omits pull request notices.

--- features/gitHubFeed.php
+++ features/gitHubFeed.php
@@ -5,8 +5,8 @@ $rss = fetch_feed( 'https://github.com/CentralCollege/departments-wordpress-them
 
 if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
 
-    // Figure out how many total items there are, but limit it to 5. 
-    $maxitems = $rss->get_item_quantity( 5 ); 
+    // Figure out how many total items there are, but limit it to 5.
+    $maxitems = $rss->get_item_quantity( 5 );
 
     // Build an array of all the items, starting with element 0 (first element).
     $rss_items = $rss->get_items( 0, $maxitems );
@@ -20,13 +20,19 @@ endif;
     <?php else : ?>
         <?php // Loop through each feed item and display each item as a hyperlink. ?>
         <?php foreach ( $rss_items as $item ) : ?>
+          <?php if(stripos($item->get_title(), 'Merge pull request') !== false){
+            $display = false;
+          }else {
+            $display = true;
+          }
+          if($display==true){?>
             <li>
                 <a href="<?php echo esc_url( $item->get_permalink() ); ?>"
                     title="<?php printf( __( 'Posted %s', 'my-text-domain' ), $item->get_date('j F Y | g:i a') ); ?>">
                     <?php echo esc_html( $item->get_title() ); ?>
                 </a>
             </li>
+          <?php } ?>
         <?php endforeach; ?>
     <?php endif; ?>
 </ul>
-
