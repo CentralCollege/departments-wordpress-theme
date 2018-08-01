f61731c7f762a2efad1399f25834df4bbca93aef
bohrj <bohrj@central.edu>
2018-08-01 15:57:45 -0500

    Adds CBE code to page and single, for good measure.

--- page.php
+++ page.php
@@ -1,25 +1,36 @@
 <?php get_header(); ?>
+<!-- begin CBE code -->
+<script>
+		(function(a,b,c,d,e,f,g) {
+		a[e] = a[e] || function() {(a[e].q = a[e].q || []).push(arguments)};f=b.createElement(c);
+		g=b.getElementsByTagName(c)[0];f.async=1;f.src=d+"/cbe/cbe.js";g.parentNode.insertBefore(f,g);
+		})(window,document,"script","https://cbe.capturehighered.net","_cbe");
+
+		_cbe("create","e1c184ce");
+		_cbe("log","pageview");
+</script>
+<!-- end CBE code -->
 
 	<div class="posts">
 	<?php
 	//Loop starts here
 	if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <div class="post-content">
-  	
+
     <h2><?php the_title() ?></h2>
-    
+
     <?php the_content(); ?>
-    
+
     <div class="clearboth"></div>
     </div>
 
-	<?php 
+	<?php
     //Loop ends here
     endwhile; else: ?>
     <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
     <?php endif; ?>
     </div>
-    
+
 	<?php get_sidebar(); ?>
-    
+
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,4 +1,15 @@
 <?php get_header(); ?>
+<!-- begin CBE code -->
+<script>
+		(function(a,b,c,d,e,f,g) {
+		a[e] = a[e] || function() {(a[e].q = a[e].q || []).push(arguments)};f=b.createElement(c);
+		g=b.getElementsByTagName(c)[0];f.async=1;f.src=d+"/cbe/cbe.js";g.parentNode.insertBefore(f,g);
+		})(window,document,"script","https://cbe.capturehighered.net","_cbe");
+
+		_cbe("create","e1c184ce");
+		_cbe("log","pageview");
+</script>
+<!-- end CBE code -->
 
 	<div class="posts">
 	<?php
@@ -6,26 +17,26 @@
 	if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <div class="post-content">
     <h2><?php the_title(); ?></h2>
-    
+
     <?php the_time( get_option( 'date_format' ) ) ?>
-    
+
     <?php the_content(); ?>
-    
+
     <hr size="1"/>
-	
-	<?php comments_template(); ?>  
-    
+
+	<?php comments_template(); ?>
+
     <div class="clearboth"></div>
-    
+
     </div>
 
-	<?php 
+	<?php
     //Loop ends here
     endwhile; else: ?>
     <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
     <?php endif; ?>
     </div>
-    
+
     <?php get_sidebar(); ?>
-	
+
 <?php get_footer(); ?>
