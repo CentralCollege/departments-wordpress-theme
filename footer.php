5bc4dff69bb2cb6c2dffd19c6e1e4b36f48685f5
Jordan Bohr <bohrj@central.edu>
2018-09-21 10:58:36 -0500

    Adds CBE code to footer

--- footer.php
+++ footer.php
@@ -28,6 +28,18 @@
         </div>
     </div>
 </div>
+<!-- begin CBE code -->
+<script>
+	(function(a,b,c,d,e,f,g) {
+	a[e] = a[e] || function() {(a[e].q = a[e].q || []).push(arguments)};f=b.createElement(c);
+	g=b.getElementsByTagName(c)[0];f.async=1;f.src=d+"/cbe/cbe.js";g.parentNode.insertBefore(f,g);
+	})(window,document,"script","https://cbe.capturehighered.net","_cbe");
+
+	_cbe("create","e1c184ce");
+	_cbe("log","pageview");
+</script>
+<!-- end CBE code -->
+
     <?php wp_footer(); ?>
 	</body>
 </html>
