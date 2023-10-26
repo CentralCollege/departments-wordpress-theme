414b23082c5c4c6bd9529205b8f8e1d4459e09a8
AC-ITInt <85520673+AC-ITInt@users.noreply.github.com>
2023-10-26 13:31:58 -0500

    Update header.php

--- header.php
+++ header.php
@@ -18,13 +18,6 @@
 				  gtag('js', new Date());
 				
 				  gtag('config', 'G-6JGG3BZLP9');
-				</script><script>
-				  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-				  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-				  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-				  ga('create', 'UA-99021860-1', 'auto');
-				  ga('send', 'pageview');
 				</script>
 				<!-- begin CBE code -->
 				<script>
