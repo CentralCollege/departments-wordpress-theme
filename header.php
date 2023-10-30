d2e8fe7247ab26cde8be9d73b27141b306ee2a81
AC-ITInt <85520673+AC-ITInt@users.noreply.github.com>
2023-10-30 10:18:50 -0500

    Update header.php

--- header.php
+++ header.php
@@ -2,14 +2,14 @@
 <html>
 	<head>
     	<meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
-        <link rel="shortcut icon" href="//www.central.edu/favicon.ico" /><!--TEST-->
+        <link rel="shortcut icon" href="//www.central.edu/favicon.ico" />
         <link rel="apple-touch-icon-precomposed" href="//d1lqhpmxg10s5j.cloudfront.net/images/main/centralCollegeLogo-57.png">
         <link rel="apple-touch-icon-precomposed" sizes="72x72" href="//d1lqhpmxg10s5j.cloudfront.net/images/main/centralCollegeLogo-72.png">
         <link rel="apple-touch-icon-precomposed" sizes="114x114" href="//d1lqhpmxg10s5j.cloudfront.net/images/main/centralCollegeLogo-114.png">
         <link rel="apple-touch-icon-precomposed" sizes="144x144" href="//d1lqhpmxg10s5j.cloudfront.net/images/main/centralCollegeLogo-144.png">
         <link href='//fonts.googleapis.com/css?family=Cagliostro' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
-				<?php if($_SERVER['HTTP_HOST'] == 'lillyfellows.central.edu'){ ?>
+				<!--TEST2-->
 				<!-- Google tag (gtag.js) -->
 				<script async src="https://www.googletagmanager.com/gtag/js?id=G-6JGG3BZLP9"></script>
 				<script>
@@ -20,6 +20,7 @@
 				  gtag('config', 'G-6JGG3BZLP9');
 				</script>
 				<!-- begin CBE code -->
+				<?php if($_SERVER['HTTP_HOST'] == 'lillyfellows.central.edu'){ ?>
 				<script>
 				    (function(a,b,c,d,e,f,g) {
 				    a[e] = a[e] || function() {(a[e].q = a[e].q || []).push(arguments)};f=b.createElement(c);
